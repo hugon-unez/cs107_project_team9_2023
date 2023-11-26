c76be7dce8e7b9a100dc9d8b3c73ec828ae622ef
Hugo Nunez <hugonunez@college.harvard.edu>
2023-11-26 00:12:30 -0600

    created file header, added initial imports

---
+++ src/group9_package/subpkg_1/core_functions_module.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# File       : core_functions_module.py
+# Description: Core functions module for spectral data analysis
+# License    : GNU General Public License, version 3
+# Copyright 2023 Harvard University. All Rights Reserved.
+
+from astroquery.sdss import SDSS
