3469a6c054290e89a0d636665e80b111ec9fc008
Hugo Nunez <hugonunez@college.harvard.edu>
2023-11-26 15:00:59 -0500

    created init function for base object

--- src/group9_package/subpkg_1/core_functions_module.py
+++ src/group9_package/subpkg_1/core_functions_module.py
@@ -5,3 +5,14 @@
 # Copyright 2023 Harvard University. All Rights Reserved.
 
 from astroquery.sdss import SDSS
+from astropy.table import Table
+
+class SpectralAnalysisBase:
+    def __init__(self, query, data=None):
+        self.query = query
+
+        # check that data is same type as query
+        if data is not None and not isinstance(data, Table):
+            raise ValueError("data must be an Astropy Table object")
+        
+        self.data = data
