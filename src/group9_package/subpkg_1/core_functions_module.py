b5331f7bc68ff909f2706170b793bd3a45b8af0d
Hugo Nunez <hugonunez@college.harvard.edu>
2023-11-26 15:51:32 -0500

    updated error handling with query validation function

--- src/group9_package/subpkg_1/core_functions_module.py
+++ src/group9_package/subpkg_1/core_functions_module.py
@@ -19,23 +19,30 @@ class SpectralAnalysisBase:
         
         self.data = data
 
+    # helper function to catch obviously invalid queries
+    def query_validation(query):
+        if 'select' not in query.lower() or 'from' not in query.lower():
+            raise ValueError("Query is invalid.")
+
     def execute_query(self):
         # use try except block in order to catch issues with query
         try:
+            self.validate_query()  # Validate the query before executing
             result = SDSS.query_sql(self.query)
             self.data = Table(result)
-        except (RemoteServiceError, TimeoutError) as e:
+        except (RemoteServiceError, TimeoutError, ValueError) as e:
             print(f"Query Error: {e}")
         except RequestException as e:
             print(f"RequestException: {e}")
         else:
             print("Query executed successfully and result stored in data attribute.")
+        
 
 
 def main():
     query = "select top 10 XYA, ra, dec, bestObjID from specObj where class = 'galaxy'  and z > 0.3 and zWarning = 0"
-    res = SDSS.query_sql(query)
-    print(res[:5])
+    analysis_instance = SpectralAnalysisBase(query)
+    analysis_instance.execute_query()
 
 if __name__ == "__main__":
     main()
