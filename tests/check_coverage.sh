f4b12a966e43c81d1ff8fd818f328f99861fb6ef
Ricardo Linares <ricardolinares209@gmail.com>
2023-11-28 17:39:30 -0500

    Completely change coverage.yml file

--- tests/check_coverage.sh
+++ tests/check_coverage.sh
@@ -2,25 +2,6 @@
 # File       : check_coverage.sh
 # Description: Coverage wrapper around test suite driver script
 # Copyright 2022 Harvard University. All Rights Reserved.
-set -e
-
-tool='coverage'
-if [[ $# -gt 0 ]]; then
-    # optional argument to use different tool to check coverage
-    tool="${1}"; shift
-fi
-
-if [[ ${tool} == 'coverage' ]]; then
-    # run the tests (generates coverage data to build report)
-    ./run_tests.sh coverage run --source=group9_package "${@}"
-
-    # build the coverage report on stdout
-    coverage report -m
-elif [[ ${tool} == 'pytest' ]]; then
-    # generate coverage reports with pytest in one go
-    ./run_tests.sh pytest --cov=group9_package "${@}"
-else
-    # error: write to stderr
-    >&2 echo "Error: unknown tool '${tool}'"
-    exit 1
-fi
+coverage_threshold=90
+coverage_result=$(cd tests && coverage run --source=group9_package -m pytest && coverage report --fail-under=$coverage_threshold)
+echo "$coverage_result"
