3b4fe729dbd63949768d75ccdd4f6b400c413c76
Ricardo Linares <ricardolinares209@gmail.com>
2023-11-28 17:53:18 -0500

    Uncomment code out to cause failure and fix check_coverage.sh to return exit code

--- tests/check_coverage.sh
+++ tests/check_coverage.sh
@@ -2,6 +2,11 @@
 # File       : check_coverage.sh
 # Description: Coverage wrapper around test suite driver script
 # Copyright 2022 Harvard University. All Rights Reserved.
+
 coverage_threshold=90
 coverage_result=$(coverage run --source=group9_package -m pytest && coverage report --fail-under=$coverage_threshold)
 echo "$coverage_result"
+# Check if coverage is below the threshold and exit with a non-zero status code if so
+if [[ $? -ne 0 ]]; then
+  exit 1
+fi
