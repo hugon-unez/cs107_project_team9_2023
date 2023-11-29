aa29337c68f6f73c7ce58a8fa599c1a68f707343
Hugo Nunez <hugonunez@college.harvard.edu>
2023-11-28 20:54:23 -0500

    debugging workflow

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -26,7 +26,7 @@ elif [[ $# -gt 0 && ${1} == 'CI' ]]; then
     unset PYTHONPATH
     driver="pytest ${@}"
 else
-    driver="python ${@} -m unittest"
+    driver="python3 ${@} -m unittest"
 fi
 
 # run the tests
