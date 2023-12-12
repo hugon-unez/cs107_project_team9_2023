ef585a809c260008c36bcaed79259b7bcefacbc8
Hugo Nunez <hugonunez@college.harvard.edu>
2023-12-11 23:36:35 -0500

    initialized visualize module

---
+++ src/group9_package/subpkg_1/visualize_module.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# File       : visualize_module.py
+# Description: Visualization module for spectral analysis
+# License    : GNU General Public License, version 3
+# Copyright 2023 Harvard University. All Rights Reserved.
+
+
+import matplotlib
+import pandas as pd
+import numpy as np
+from core_functions_module_extract import SpectraExtract
+from astropy.table import Table
