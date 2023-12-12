a532a32e21ac810157acc0de5460e3cac29123c4
Hugo Nunez <hugonunez@college.harvard.edu>
2023-12-11 23:56:54 -0500

    wrote init function for spectralvisualizer

--- src/group9_package/subpkg_1/visualize_module.py
+++ src/group9_package/subpkg_1/visualize_module.py
@@ -10,3 +10,16 @@ import pandas as pd
 import numpy as np
 from core_functions_module_extract import SpectraExtract
 from astropy.table import Table
+
+class SpectralVisualizer:
+    def __init__(self, *, row=None, data=None):
+        if (row is not None and not isinstance(row, Table.Row)) or (data is not None and not isinstance(data, pd.DataFrame)):
+            raise TypeError("Invalid type for parameter. 'row' should be Table.Row or 'data' should be a pandas DataFrame.")
+
+
+        if (row is not None and data is not None) or (row is None and data is None):
+            raise ValueError("Either 'row' or 'data' should be provided, but not both or neither.")
+
+
+        self.row = row
+        self.data = data if data is not None else pd.DataFrame()  # Initialize data attribute
