b3fa2e699baafa56cb96ea5e35e3e76fe9d3f6b8
Zach Stack <zstack@college.harvard.edu>
2023-12-13 15:58:09 -0600

    Add Paul's DataPreprocessor Work to appropriate files

--- src/group9_package/subpkg_1/core_functions_module_extract.py
+++ src/group9_package/subpkg_1/core_functions_module_extract.py
@@ -8,6 +8,9 @@ from astroquery.sdss import SDSS
 from astropy.table import Table
 from astroquery.exceptions import RemoteServiceError, TimeoutError
 from requests.exceptions import RequestException
+from scipy.interpolate import interp1d
+from scipy.stats import zscore
+import numpy as np
 import pandas as pd
 import requests
 import io
@@ -143,3 +146,92 @@ class SpectraExtract(SpectralAnalysisBase):
 
         #print failure message
         print(f'Request failed after {retries} retries. Ensure proper row was input or try again later.')
+
+class DataPreprocessor(SpectralAnalysisBase):
+    #Spectral Data:
+    #Definition: Spectral data represents how the intensity of light emitted or received 
+    #by an object varies across different wavelengths. Representation: A spectrum is often 
+    #presented as a graph where the x-axis represents the wavelength (or frequency) of light
+    #and the y-axis represents the intensity or flux of light at each wavelength.
+
+    #For light waves, the wavelength corresponds to the distance between two successive 
+    #peaks or troughs of the electromagnetic wave.
+
+    #User is expected to provide the query
+    def __init__(self, query, data):
+
+        #Similar to pp6 in that we're turning the query into a pandas dataframe
+        self.query = query
+
+        if not isinstance(data, pd.DataFrame):
+            raise ValueError("Input data must be a pandas DataFrame")
+
+
+        self.data = data
+
+        job = SDSS.query_sql(self.query)
+        #r = job.get_results()
+        self.data = job.to_pandas()
+
+        self.column_headers = list(self.data.columns)
+
+    def normalize_data(self):
+        if self.data is not None:
+            # Perform normalization on the data 
+            #looping through each column and normalizing each one and replacing them 
+            for header in self.column_headers:
+                normalized_flux_data = (self.data[header] - np.mean(self.data[header])) / np.std(self.data[header])
+                #Resetting what the column is equal to
+                self.data[header] = normalized_flux_data
+        else:
+            raise ValueError("No data available for normalization")
+
+    def remove_outliers(self, threshold=2.5):
+        if self.data is not None:
+            for header in self.column_headers:
+                # Remove outliers from data in using z-score
+                z_scores = np.abs(zscore(self.data[header]))
+                outliers_removed = self.data[header][z_scores < threshold]
+                self.data[header] = outliers_removed
+        else:
+            raise ValueError("No data available for outlier removal")
+
+    #Interpolation is commonly employed when you have a set of discrete data points 
+    #and you want to estimate the values at positions that are not explicitly provided.
+    def interpolate_data(self, new_wavelengths):
+        if self.data is not None:
+            if len(new_wavelengths) != len(self.data.index):
+                raise ValueError("Length of new_wavelengths does not match length of index")
+            
+            for header in self.column_headers:
+                #spectral data probably observes non-linear relationship
+                interp_function = interp1d(self.data.index, self.data[header], kind='cubic', fill_value="extrapolate", bounds_error=False )
+
+                # Use the interpolation function to estimate values at new_wavelengths
+                interpolated_values = interp_function(new_wavelengths)
+
+                # Update the dataframe with the interpolated values
+                self.data[header] = interpolated_values
+        else:
+            raise ValueError("No data available for interpolation")
+
+    #the light emitted by distant objects undergoes a redshift, meaning that the wavelengths of the 
+    #Emitted light are stretched and shifted towards the longer, "red" end of the electromagnetic spectrum.
+    #the redshift of an object is directly proportional to its distance from an observer due to the expansion 
+    # of the universe. The farther an object is, the greater its redshift tends to be.
+
+    def correct_redshift(self, bands=['u', 'g', 'r', 'i']):
+        if self.data is not None:
+            # Adjust wavelengths in SpecObjAll based on redshift values
+            #Original spectra is emit
+            #observed is what happens after spectra has been affected by redshift
+            #redshift correction is retrieving original spectra
+            #wikipedia says equation is (1+z = obs. wavelength / emitted wavelength)
+            #Rearranged equation to get emitted wavelength
+            for band in bands:
+                if band not in self.data.columns:
+                    continue  # Skip bands not present in the data
+                corrected_values = self.data[band] / (1 + self.data['z'])
+                self.data[band] = corrected_values
+        else:
+            raise ValueError("No wavelength data available for redshift correction")
--- tests/subpkg_1/test_unittests_core_function_module.py
+++ tests/subpkg_1/test_unittests_core_function_module.py
@@ -1,7 +1,9 @@
 import unittest
 from unittest.mock import patch
 from astropy.table import Table
-from group9_package.subpkg_1.core_functions_module_extract import SpectralAnalysisBase, MetaDataExtractor
+import pandas as pd
+import numpy as np
+from group9_package.subpkg_1.core_functions_module_extract import SpectralAnalysisBase, MetaDataExtractor, DataPreprocessor
 from astroquery.exceptions import RemoteServiceError, TimeoutError
 from requests.exceptions import RequestException
 
@@ -117,6 +119,72 @@ class TestMetaDataExtractor(unittest.TestCase):
         extractor = MetaDataExtractor(self.invalid_query)
         with self.assertRaises(ValueError):
             extractor.extract_redshifts()
+
+class TestDataPreprocessor(unittest.TestCase):
+    def setUp(self):
+        self.valid_query = "select top 10 ra, dec, bestObjID from specObj where class = 'galaxy' and z > 0.3 and zWarning = 0"
+        self.invalid_query = "Invalid Query"
+
+    # Test initialization with valid data
+    @patch('group9_package.subpkg_1.core_functions_module_extract.SDSS.query_sql')
+    def test_init_with_valid_data(self, mock_query_sql):
+        result_data = pd.DataFrame({'ra': [1, 2, 3], 'dec': [4, 5, 6], 'u': [7, 8, 9], 'g': [10, 11, 12], 'r': [13, 14, 15], 'i': [16, 17, 18], 'z': [0.4, 0.5, 0.6]})
+        mock_query_sql.return_value = result_data
+
+        data = pd.DataFrame()  # Use pandas DataFrame here
+        base = SpectralAnalysisBase(self.valid_query, data=data)
+        self.assertEqual(base.query, self.valid_query)
+        self.assertIsInstance(base.data, pd.DataFrame)
+
+    # Test initialization with invalid data
+    def test_init_with_invalid_data(self):
+        with self.assertRaises(ValueError):
+            DataPreprocessor(self.valid_query, data="invalid_data")
+
+    # Test normalization
+    def test_normalize_data(self):
+        data = pd.DataFrame({'ra': [1, 2, 3], 'u': [7, 8, 9], 'g': [10, 11, 12], 'r': [13, 14, 15], 'i': [16, 17, 18], 'z': [0.4, 0.5, 0.6]})
+        data_preprocessor = DataPreprocessor(self.valid_query, data=data)
+        data_preprocessor.normalize_data()
+
+        for header in data_preprocessor.column_headers:
+            normalized_data = (data[header] - np.mean(data[header])) / np.std(data[header])
+            np.testing.assert_array_almost_equal(data_preprocessor.data[header], normalized_data)
+
+    # Test outlier removal
+    def test_remove_outliers(self):
+        data = pd.DataFrame({'ra': [1, 2, 3], 'u': [7, 8, 9], 'g': [10, 11, 12], 'r': [13, 14, 15], 'i': [16, 17, 18], 'z': [0.4, 0.5, 0.6]})
+        data_preprocessor = DataPreprocessor(self.valid_query, data=data)
+        data_preprocessor.remove_outliers()
+
+        for header in data_preprocessor.column_headers:
+            z_scores = np.abs((data[header] - np.mean(data[header])) / np.std(data[header]))
+            outliers_removed = data[header][z_scores < 2.5]
+            np.testing.assert_array_almost_equal(data_preprocessor.data[header], outliers_removed)
+
+    # Test interpolation
+    def test_interpolate_data(self):
+        data = pd.DataFrame({'ra': [1, 2, 3], 'u': [7, 8, 9], 'g': [10, 11, 12], 'r': [13, 14, 15], 'i': [16, 17, 18], 'z': [0.4, 0.5, 0.6]})
+        data_preprocessor = DataPreprocessor(self.valid_query, data=data)
+
+        new_wavelengths = np.array([1.1, 1.2, 1.3, 1.4])
+        data_preprocessor.interpolate_data(new_wavelengths)
+
+        for header in data_preprocessor.column_headers:
+            interp_function = np.interp(new_wavelengths, data.index, data[header])
+            np.testing.assert_array_almost_equal(data_preprocessor.data[header], interp_function)
+
+    # Test redshift correction
+    def test_correct_redshift(self):
+        data = pd.DataFrame({'ra': [1, 2, 3], 'u': [7, 8, 9], 'g': [10, 11, 12], 'r': [13, 14, 15], 'i': [16, 17, 18], 'z': [0.4, 0.5, 0.6]})
+        data_preprocessor = DataPreprocessor(self.valid_query, data=data)
+
+        bands = ['u', 'g', 'r', 'i']
+        data_preprocessor.correct_redshift(bands)
+
+        for band in bands:
+            corrected_values = data[band] / (1 + data['z'])
+            np.testing.assert_array_almost_equal(data_preprocessor.data[band], corrected_values)
 
 if __name__ == '__main__':
     unittest.main()
