f8a1022f88aca659f93ca46fb64d3c668d4f821e
Zach Stack <zstack@college.harvard.edu>
2023-12-14 13:01:06 -0600

    Push Unit Tests - Extrapolate one

--- tests/subpkg_1/test_unittests_core_function_modify.py
+++ tests/subpkg_1/test_unittests_core_function_modify.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 from astropy.table import Table
 import pandas as pd
 import numpy as np
+from scipy.interpolate import interp1d
 from group9_package.subpkg_1.core_functions_module_extract import SpectralAnalysisBase
 from group9_package.subpkg_1.core_functions_module_modify import DataPreprocessor
 
@@ -11,35 +12,41 @@ class TestDataPreprocessor(unittest.TestCase):
         self.valid_query = "select top 10 ra, dec, bestObjID from specObj where class = 'galaxy' and z > 0.3 and zWarning = 0"
         self.invalid_query = "Invalid Query"
 
-    # Test initialization with valid data
-    @patch('group9_package.subpkg_1.core_functions_module_extract.SDSS.query_sql')
-    def test_init_with_valid_data(self, mock_query_sql):
-        result_data = pd.DataFrame({'ra': [1, 2, 3], 'dec': [4, 5, 6], 'u': [7, 8, 9], 'g': [10, 11, 12], 'r': [13, 14, 15], 'i': [16, 17, 18], 'z': [0.4, 0.5, 0.6]})
-        mock_query_sql.return_value = result_data
-
-        data = pd.DataFrame()  # Use pandas DataFrame here
-        base = SpectralAnalysisBase(self.valid_query, data=data)
-        self.assertEqual(base.query, self.valid_query)
-        self.assertIsInstance(base.data, pd.DataFrame)
-
     # Test initialization with invalid data
     def test_init_with_invalid_data(self):
         with self.assertRaises(ValueError):
             DataPreprocessor(self.valid_query, data="invalid_data")
 
     # Test normalization
-    def test_normalize_data(self):
+    @patch('group9_package.subpkg_1.core_functions_module_extract.SDSS.query_sql')
+    def test_normalize_data(self, mock_query_sql):
         data = pd.DataFrame({'ra': [1, 2, 3], 'u': [7, 8, 9], 'g': [10, 11, 12], 'r': [13, 14, 15], 'i': [16, 17, 18], 'z': [0.4, 0.5, 0.6]})
+        mock_query_sql.return_value = Table.from_pandas(data)
         data_preprocessor = DataPreprocessor(self.valid_query, data=data)
         data_preprocessor.normalize_data()
 
         for header in data_preprocessor.column_headers:
             normalized_data = (data[header] - np.mean(data[header])) / np.std(data[header])
+            print(normalized_data)
+            print("zach")
+            print(data_preprocessor.data[header])
             np.testing.assert_array_almost_equal(data_preprocessor.data[header], normalized_data)
 
-    # Test outlier removal
-    def test_remove_outliers(self):
+    # Test remove normalize with invalid data
+    @patch('group9_package.subpkg_1.core_functions_module_extract.SDSS.query_sql')
+    def test_remove_normalize_with_invalid_data(self, mock_query_sql):
         data = pd.DataFrame({'ra': [1, 2, 3], 'u': [7, 8, 9], 'g': [10, 11, 12], 'r': [13, 14, 15], 'i': [16, 17, 18], 'z': [0.4, 0.5, 0.6]})
+        mock_query_sql.return_value = Table.from_pandas(data)
+        with self.assertRaises(ValueError):
+            dataPre = DataPreprocessor(self.valid_query, data=data)
+            dataPre.data = None
+            dataPre.normalize_data()
+
+    # Test outlier removal
+    @patch('group9_package.subpkg_1.core_functions_module_extract.SDSS.query_sql')
+    def test_remove_outliers(self, mock_query_sql):
+        data = pd.DataFrame({'ra': [1, 2, 3], 'u': [7, 8, 9], 'g': [10, 11, 12], 'r': [13, 14, 15], 'i': [16, 17, 18], 'z': [0.4, 0.5, 0.6]})
+        mock_query_sql.return_value = Table.from_pandas(data)
         data_preprocessor = DataPreprocessor(self.valid_query, data=data)
         data_preprocessor.remove_outliers()
 
@@ -48,21 +55,50 @@ class TestDataPreprocessor(unittest.TestCase):
             outliers_removed = data[header][z_scores < 2.5]
             np.testing.assert_array_almost_equal(data_preprocessor.data[header], outliers_removed)
 
-    # Test interpolation
-    def test_interpolate_data(self):
+    # Test remove outliers with invalid data
+    @patch('group9_package.subpkg_1.core_functions_module_extract.SDSS.query_sql')
+    def test_remove_outliers_with_invalid_data(self, mock_query_sql):
         data = pd.DataFrame({'ra': [1, 2, 3], 'u': [7, 8, 9], 'g': [10, 11, 12], 'r': [13, 14, 15], 'i': [16, 17, 18], 'z': [0.4, 0.5, 0.6]})
+        mock_query_sql.return_value = Table.from_pandas(data)
+        with self.assertRaises(ValueError):
+            dataPre = DataPreprocessor(self.valid_query, data=data)
+            dataPre.data = None
+            dataPre.remove_outliers()
+
+    # Test interpolation
+    @patch('group9_package.subpkg_1.core_functions_module_extract.SDSS.query_sql')
+    def test_interpolate_data(self, mock_query_sql):
+        data = pd.DataFrame({'ra': [1, 2, 3], 'u': [7, 8, 9], 'g': [10, 11, 12], 'r': [13, 14, 15], 'i': [16, 17, 18], 'z': [0.4, 0.5, 0.6]})
+        mock_query_sql.return_value = Table.from_pandas(data)
         data_preprocessor = DataPreprocessor(self.valid_query, data=data)
 
-        new_wavelengths = np.array([1.1, 1.2, 1.3, 1.4])
+        print(np.arange(0, 10))
+        print(np.exp(-np.arange(0, 10)/3.0))
+
+        new_wavelengths = np.array([1, 1.2, 1.3])
         data_preprocessor.interpolate_data(new_wavelengths)
 
         for header in data_preprocessor.column_headers:
-            interp_function = np.interp(new_wavelengths, data.index, data[header])
+            #interp_function = np.interp(new_wavelengths, data.index, data[header])
+            interp_function = interp1d(data.index, data[header], kind='cubic', fill_value="extrapolate", bounds_error=False)
             np.testing.assert_array_almost_equal(data_preprocessor.data[header], interp_function)
 
-    # Test redshift correction
-    def test_correct_redshift(self):
+    # Test interpolation with invalid data
+    @patch('group9_package.subpkg_1.core_functions_module_extract.SDSS.query_sql')
+    def test_interpolation_with_invalid_data(self, mock_query_sql):
         data = pd.DataFrame({'ra': [1, 2, 3], 'u': [7, 8, 9], 'g': [10, 11, 12], 'r': [13, 14, 15], 'i': [16, 17, 18], 'z': [0.4, 0.5, 0.6]})
+        mock_query_sql.return_value = Table.from_pandas(data)
+        with self.assertRaises(ValueError):
+            dataPre = DataPreprocessor(self.valid_query, data=data)
+            dataPre.data = None
+            new_wavelengths = np.array([1, 1.2, 1.3])
+            dataPre.interpolate_data(new_wavelengths)
+    
+    # Test redshift correction
+    @patch('group9_package.subpkg_1.core_functions_module_extract.SDSS.query_sql')
+    def test_correct_redshift(self, mock_query_sql):
+        data = pd.DataFrame({'ra': [1, 2, 3], 'u': [7, 8, 9], 'g': [10, 11, 12], 'r': [13, 14, 15], 'i': [16, 17, 18], 'z': [0.4, 0.5, 0.6]})
+        mock_query_sql.return_value = Table.from_pandas(data)
         data_preprocessor = DataPreprocessor(self.valid_query, data=data)
 
         bands = ['u', 'g', 'r', 'i']
@@ -71,3 +107,13 @@ class TestDataPreprocessor(unittest.TestCase):
         for band in bands:
             corrected_values = data[band] / (1 + data['z'])
             np.testing.assert_array_almost_equal(data_preprocessor.data[band], corrected_values)
+
+    # Test redshifts with invalid data
+    @patch('group9_package.subpkg_1.core_functions_module_extract.SDSS.query_sql')
+    def test_redshift_with_invalid_data(self, mock_query_sql):
+        data = pd.DataFrame({'ra': [1, 2, 3], 'u': [7, 8, 9], 'g': [10, 11, 12], 'r': [13, 14, 15], 'i': [16, 17, 18], 'z': [0.4, 0.5, 0.6]})
+        mock_query_sql.return_value = Table.from_pandas(data)
+        with self.assertRaises(ValueError):
+            dataPre = DataPreprocessor(self.valid_query, data=data)
+            dataPre.data = None
+            dataPre.correct_redshift()
