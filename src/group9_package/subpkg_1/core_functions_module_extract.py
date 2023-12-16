6f62bce16757c56b02a91b71722844f6c04cf234
Ricardo Linares <ricardolinares209@gmail.com>
2023-12-16 13:46:23 -0600

    Fix subpkg_1 module docstrings

--- src/group9_package/subpkg_1/core_functions_module_extract.py
+++ src/group9_package/subpkg_1/core_functions_module_extract.py
@@ -3,6 +3,7 @@
 # Description: Core functions module for spectral data analysis
 # License    : GNU General Public License, version 3
 # Copyright 2023 Harvard University. All Rights Reserved.
+"""This python module provides functionality for querying and extracting astronomical data from the SDSS database. """
 
 from astroquery.sdss import SDSS
 from astropy.table import Table
@@ -14,13 +15,17 @@ import io
 import time
 
 class SpectralAnalysisBase:
+    """Foundational class for performing spectral analysis by querying and handling data from the SDSS database."""
     def __init__(self, query, data=None):
-        """Initializes the base class for spectral analysis class
+        """Initializes SpectralAnalysisBase Class
 
         Args:
             query (str): String parameter containing an ADQL query to query the SDSS database.
             data (astropy.table.Table, optional): Optional parameter to input spectral data as an Astropy Table object.
                 Defaults to None.
+
+        Raises:
+            ValueError: If the given data is not an astropy table
         """
         self.query = query
 
@@ -30,13 +35,25 @@ class SpectralAnalysisBase:
         
         self.data = data
 
-    # helper function to catch obviously invalid queries
     @staticmethod
     def query_validation(query:str):
+        """Validates the given query string to ensure it contains basic elements of a SQL query.
+
+        Args:
+            query (str): The query string to validate.
+
+        Raises:
+            ValueError: If the query string does not contain necessary SQL elements.
+        """
         if 'select' not in query.lower() or 'from' not in query.lower():
             raise ValueError("Query is invalid.")
 
     def execute_query(self):
+        """Executes the SQL query against the SDSS database and stores the result in the data attribute.
+
+        Raises:
+            RemoteServiceError, TimeoutError, ValueError, RequestException: For various errors that may occur during query execution.
+        """
         # use try except block in order to catch issues with query
         try:
             self.query_validation(self.query)  # Validate the query before executing
@@ -52,18 +69,26 @@ class SpectralAnalysisBase:
             print("Query executed successfully and result stored in data attribute.")
         
 class MetaDataExtractor(SpectralAnalysisBase):
+    """A Class for extracting user requested metadata"""
     def __init__(self, query, data=None):
-        # Initializes MetadataExtractor class
+        """Initializes MetadataExtractor Class
 
-        #    Args:
-        #     query (str): String parameter containing an ADQL query to query the SDSS database
-        #     data (astropy.table.Table, optional): Optional parameter to input spectral data as Astropy Table object
-        #         Default = None.
-        
+        Args:
+            query (str): String parameter containing an ADQL query to query the SDSS database
+            data (astropy.table.Table, optional): Optional parameter to input spectral data 
+                as Astropy Table object. Default = None.
+        """
         super().__init__(query, data)
 
     def extract_identifiers(self):
-        # extracts identifiers from the data
+        """Extracts unique identifiers from the data.
+
+        Returns:
+            Astropy Table Column: A column of identifiers from the data.
+
+        Raises:
+            ValueError: If no data is available to extract identifiers or the 'bestObjID' column is missing.
+        """        
         if self.data is None or 'bestObjID' not in self.data.colnames:
             raise ValueError("No data available to extract identifiers.")
         
@@ -71,7 +96,14 @@ class MetaDataExtractor(SpectralAnalysisBase):
         return identifiers
 
     def extract_coordinates(self):
-        # extracts coordinates from the data
+        """Extracts astronomical coordinates from the data.
+
+        Returns:
+            Astropy Table: A table containing the 'bestObjID', 'ra', and 'dec' columns.
+
+        Raises:
+            ValueError: If no data is available to extract coordinates or the required columns are missing.
+        """
         coordinatesCol = ['bestObjID', 'ra', 'dec']
         if self.data is None or not all(item in self.data.colnames  for item in coordinatesCol):
             raise ValueError("No data available to extract coordinates.")
@@ -79,10 +111,15 @@ class MetaDataExtractor(SpectralAnalysisBase):
         coordinates = self.data['bestObjID', 'ra', 'dec']
         return coordinates
 
-    # below needs to be adjusted for correct chemical abundance colums
     def extract_chemical_abundances(self):
-        # extracts chemical abundances 
-        # assume the chemical abundances are stored in specific columns in the data
+        """Extracts chemical abundances from the data.
+
+        Returns:
+            Astropy Table: A table containing the 'bestObjID' and 'elodieFeH' columns.
+
+        Raises:
+            ValueError: If no data is available to extract chemical abundances or the required columns are missing.
+        """        
         chemicalAbundancesCol = ['bestObjID', 'elodieFeH']
         if self.data is None or not all(item in self.data.colnames  for item in chemicalAbundancesCol):
             raise ValueError("No data available to extract chemical abundances.")
@@ -92,7 +129,14 @@ class MetaDataExtractor(SpectralAnalysisBase):
         return chemical_abundances
 
     def extract_redshifts(self):
-        # extracts redshift values
+        """Extracts redshift values from the data.
+
+        Returns:
+            Astropy Table: A table containing the 'bestObjID' and 'elodieZ' columns.
+
+        Raises:
+            ValueError: If no data is available to extract redshifts or the required columns are missing.
+        """        
         redshiftsCol = ['bestObjID','elodieZ'] 
         if self.data is None or not all(item in self.data.colnames for item in redshiftsCol):
             raise ValueError("No data available to extract redshifts.")
@@ -101,7 +145,17 @@ class MetaDataExtractor(SpectralAnalysisBase):
         return redshifts
 
 class SpectraExtract(SpectralAnalysisBase):
+    """A Class for extracting spectral data for individual astronomical objects"""
     def __init__(self, data_row):
+        """Initializes the SpectraExtract Class
+
+        Args:
+            data_row (Table.Row): A single row from an Astropy Table representing an astronomical object.
+
+        Raises:
+            TypeError: If the input is not an astropy.table.Row.
+            ValueError: If the input row is missing required columns.
+        """
         if not isinstance(data_row, Table.Row):
             raise TypeError("The input must be an astropy.table.Row")
     
@@ -117,6 +171,11 @@ class SpectraExtract(SpectralAnalysisBase):
         self.row = data_row
         
     def extract_spectra(self):
+        """Retrieves spectral data for the astronomical object represented by inputted data row.
+
+        Returns:
+            DataFrame: A Pandas DataFrame containing the spectral data.
+        """
         # Initialize values to query
         row = self.row
         plate = row['plate']
