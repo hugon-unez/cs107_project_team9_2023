7f2c821345af9c038c1514da146600b4175bbb37
Ricardo Linares <ricardolinares209@gmail.com>
2023-12-16 22:08:38 -0600

    Implement unittess for machine learning module

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -9,8 +9,9 @@ tests=(
     subpkg_1/test_core_functions_module_extract.py
     subpkg_1/test_unit_tests_visualize_module.py
     subpkg_1/test_unit_tests_core_functions_module_extract.py
-    subpkg_2/test_machine_learning_module.py
     subpkg_1/test_unit_tests_core_functions_module_modify.py
+    subpkg_2/test_machine_learning_module.py
+    subpkg_2/test_unit_tests_machine_learning_module.py
 )
 
 # Must add the module source path because we use `import group9_package` in
