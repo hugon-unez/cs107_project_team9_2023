eae5b3c2fc0a28cd96a602964a89650c24cb8ae0
Zach Stack <zstack@college.harvard.edu>
2023-12-17 12:00:31 -0600

    Fix init function for queried data

--- tests/subpkg_1/test_core_functions_module_modify.py
+++ tests/subpkg_1/test_core_functions_module_modify.py
@@ -16,37 +16,30 @@ class TestDataPreprocessor(unittest.TestCase):
         Data is passed in to ensure results integrate well together
         """
         self.query = "select top 10 ra, dec, bestObjID from specObj where class = 'galaxy' and z > 0.3 and zWarning = 0"
-        self.test_data = pd.DataFrame({
-            'wavelength': np.arange(10),
-            'flux': np.random.rand(10),
-            'z': np.random.rand(10)
-        })
+        
+        self.data_preprocessor = DataPreprocessor(query=self.query, data=None)
     
     def test_data_preprocessing_pipeline(self):
         """Tests All Data Preprocessing Methods work together
         
         Confirm Equivalence and values for each method and column
         """
-        data_preprocessor = DataPreprocessor(query=self.query, data=self.test_data.copy())
 
-        data_preprocessor.normalize_data()
-        data_preprocessor.remove_outliers()
+        self.data_preprocessor.normalize_data()
+        self.data_preprocessor.remove_outliers()
 
-        data_preprocessor.interpolate_data([2,4,6,8,10,12,14,16,18,20])
+        self.data_preprocessor.interpolate_data([2,4,6,8,10,12,14,16,18,20])
 
-        data_preprocessor.correct_redshift()
+        self.data_preprocessor.correct_redshift()
 
         #assert every column 'flux' row is populated
-        self.assertTrue(all(np.isfinite(data_preprocessor.data['flux'])))
+        self.assertTrue(all(np.isfinite(self.data_preprocessor.data['ra'])))
 
         #assert every column 'wavelength' row is populated
-        self.assertTrue(all(np.isfinite(data_preprocessor.data['wavelength'])))
+        self.assertTrue(all(np.isfinite(self.data_preprocessor.data['dec'])))
 
-        #assert every column 'z' row is populated
-        self.assertTrue(all(np.isfinite(data_preprocessor.data['z'])))
-
-        #assert first row value is equivalent to what we expect
-        self.assertAlmostEqual(data_preprocessor.data['wavelength'][0], -1.5666699, places=4)
+        #assert table has correct shape
+        self.assertTrue(self.data_preprocessor.data.shape[0], 10)
 
 if __name__ == '__main__':
     unittest.main()
